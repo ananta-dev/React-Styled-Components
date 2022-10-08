a3dc7667e6e7ced3f30e035ce08cb222cd66d441
Juan J Guirao Garcia <juan.jose.guirao@gmail.com>
2022-10-08 08:10:45 +0100

    Finished!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import StyledButton, {
+    FancyButton,
+    SubmitButton,
+    AnimatedLogo,
+    DarkButton,
+} from './components/Button/Button'
+
+const theme = {
+    dark: {
+        primary: '#000',
+        text: '#FFF',
+    },
+    light: {
+        primary: '#FFF',
+        text: '#000',
+    },
+    fontFamily: 'Segoe UI',
 }
 
-export default App;
+const GlobalStyle = createGlobalStyle`
+    button {
+        font-family: ${props => props.theme.fontFamily}
+    }
+`
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <div className='App'>
+                {/* <img src={logo} alt='logo' className='App-logo' /> */}
+                <AnimatedLogo src={logo} />
+                <StyledButton>Styled Button</StyledButton>
+                <div>
+                    <br />
+                </div>
+
+                <StyledButton variant='outline'>Outline Button</StyledButton>
+                <div>
+                    <br />
+                </div>
+                <FancyButton as='a'>Fancy Button</FancyButton>
+                <div>
+                    <br />
+                </div>
+                <SubmitButton>Submit</SubmitButton>
+                <div>
+                    <br />
+                </div>
+                <SubmitButton variant='outline'>Submit Outline</SubmitButton>
+                <div>
+                    <br />
+                </div>
+                <DarkButton>Dark Theme</DarkButton>
+            </div>
+        </ThemeProvider>
+    )
+}
+
+export default App
